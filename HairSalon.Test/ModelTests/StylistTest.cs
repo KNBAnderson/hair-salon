d195c10e2149486d59891ba6e35fd9f22904cf06
Katlin <katlin.nbanderson@gmail.com>
2019-05-10 12:27:27 -0700

    fix connection error to MySQL

--- HairSalon.Test/ModelTests/StylistTest.cs
+++ HairSalon.Test/ModelTests/StylistTest.cs
@@ -10,7 +10,7 @@ namespace HairSalon.Tests
     {
         public CategoryTests()
         {
-            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=katlin_anderson_test;";
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;port=8889;database=katlin_anderson_test;default command timeout=50;";
         }
 
         [TestMethod]
